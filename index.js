6698aae140e0c3552167b75b6478c9a44ec3925f
CharlesL-Sec <173039445+CharlesL-Sec@users.noreply.github.com>
2024-09-20 17:13:28 +0100

    Updated to use function call for business rule

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 ```javascript
-// Based on script from - https://www.servicenow.com/community/developer-forum/assign-a-incident-automatically-to-a-user-in-group/m-p/1529430
- var groupName = 'Service Desk';
-// Get a list of members for this group
-var members = [];
-var gm = new GlideRecord('sys_user_grmember');
-gm.addQuery('user.active', true);
-gm.addQuery('group.name', groupName);
-gm.query();
- while (gm.next()) {
-  members.push(String(gm.user));
+function getAssignee() {
 
 // Based on script from - https://www.servicenow.com/community/developer-forum/assign-a-incident-automatically-to-a-user-in-group/m-p/1529430
  var groupName = 'soc_responder';
@@ -55,45 +46,11 @@ var userID = members[index];
 // Log their name to verify
 var user = new GlideRecord('sys_user');
 if (user.get(userID)) {
-  gs.log('Name: ' + user.name);
+  // gs.log('Name: ' + user.name);
+  return ('Name: ' + user.name);
   }
-}
-// Get a list of their open ticket counts
-var counts = [], agg, count;
-for (var i = 0; i < members.length; i++) {
-  count = 0;
-  agg = new GlideAggregate('incident');
-  agg.addActiveQuery();
-  agg.addQuery('assignment_group.name', groupName);
-  agg.addQuery('assigned_to', members[i]);
-  agg.addAggregate('COUNT');
-  agg.query();
-  if (agg.next())
-    count = agg.getAggregate('COUNT');
-    counts.push(count);
-   }
 
-
-   // find the minimum count and store its index
-
-
-   // we cannot use .sort().shift() or we won't know where to look in the members array
-
-var min = counts[0];
-var index = 0;
-for (var j = 1; j < counts.length; j++) {
-  if (counts[j] < min) {
-   min = parseInt(counts[j]);
-   index = parseInt(j);
-   }
 }
 
-
-// get the member with the lowest count
-var userID = members[index];
-// Log their name to verify
-var user = new GlideRecord('sys_user');
-if (user.get(userID)) {
-  gs.log('Name: ' + user.name);
-  }
+gs.info(getAssignee());
 ```
